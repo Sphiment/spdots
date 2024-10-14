2376656a2cf27e38b0e61ef7773b1d9352b00671
sphiment <sphiment@gmail.com>
2024-10-14 23:19:03 +0300

    confs

--- install-intel-drivers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Intel GPU drivers for 64-bit applications
-echo "Installing Intel GPU drivers..."
-sudo pacman -S --noconfirm mesa vulkan-intel intel-media-driver
-
-echo "Intel GPU drivers installed successfully!"
-
-
